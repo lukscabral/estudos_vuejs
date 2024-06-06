27a57efdfbe5729f419b1a73817e72714b23f49c
lucas <lukscabral95@gmail.com>
2024-06-05 22:09:30 -0300

    lifecycle-hooks

--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -1,9 +1,17 @@
 <template>
   <h1>Olá Vue!</h1>
+  <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
 </template>
 
 <script>
   export default {
-    name: 'PrimeiroComponente'
+    name: 'PrimeiroComponente',
+    data() {
+      return {
+        nome: 'Lucas',
+        profissao: 'programador'
+      }
+    }
+
   }
 </script>
