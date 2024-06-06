3ffe18adf4cb1b71b52dfe61b29271377c18bfe5
lucas <lukscabral95@gmail.com>
2024-06-05 23:17:54 -0300

    components-hierarchy

---
+++ src/components/Form.vue
@@ -0,0 +1,21 @@
+<template>
+    <h2>Meu formulario:</h2>
+    <form action="">
+        <div><InputText /></div>
+        <div><InputText /></div>
+        <div><Submit /></div>
+    </form>
+</template>
+
+<script>
+    import InputText from './forms/InputText.vue'
+    import Submit from './forms/Submit.vue'
+
+    export default {
+        name: 'Form',
+        components: {
+            InputText,
+            Submit
+        }
+    }
+</script>
