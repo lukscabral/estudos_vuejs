6cc9042dbe432b0777f5801c749aa7009ea6d8e5
lucas <lukscabral95@gmail.com>
2024-06-09 09:35:13 -0300

    dynamic attributes

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -8,16 +8,24 @@
         <li>Vue</li>
     </ul>
     <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+    <p>Para acessar meu portifolio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a></p>
+    <Picture />
 </template>
 
 <script>
+import Picture from './Picture.vue'
+
 export default {
     name: 'Info',
+    components: {
+        Picture
+    },
     data() {
         return {
             esta_trabalhando: false,
             mostrar_email: true,
-            email: 'lucas@email.com'
+            email: 'lucas@email.com',
+            meu_link: 'https://google.com'
         }
     }
 }
---
+++ src/components/Picture.vue
@@ -0,0 +1,15 @@
+<template>
+    <img :src="avatar" :alt="descricao"><!-- ":" é a forma abreviada de v-bind -->
+</template>
+
+<script>
+export default {
+    name: 'Picture',
+    data() {
+        return {
+            avatar: "/img/avatar.png",
+            descricao: "Lucas Cabral"
+        }
+    }
+}
+</script>
