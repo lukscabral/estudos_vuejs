26db2335892eb2faf973de739bb22b13353f43dd
lucas <lukscabral95@gmail.com>
2024-06-09 11:03:18 -0300

    css scopes

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <template>
+  <Header />
   <PrimeiroComponente />
   <LifeCycle />
   <Pessoa />
 </template>
 
 <script>
+import Header from './components/Header.vue'
 import PrimeiroComponente from './components/PrimeiroComponente.vue'
 import LifeCycle from './components/LifeCycle.vue'
 import Pessoa from './components/Pessoa.vue'
 
+
   export default {
     name: 'app',
     components: {
+      Header,
       PrimeiroComponente,
       LifeCycle,
-      Pessoa
+      Pessoa,
+      
     }
   }
 </script>
+
+<style>
+  body {
+    background-color: #333;
+    color: #fff;
+  }
+
+</style>
