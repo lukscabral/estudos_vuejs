8ddf625a6506d7437c0b9a2a1546cb215452b939
lucas <lukscabral95@gmail.com>
2024-06-10 21:58:30 -0300

    multiple events

--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -1,11 +1,17 @@
 <template>
   <h1>Olá Vue!</h1>
   <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+  <MultiplosEventos/>
 </template>
 
 <script>
+  import MultiplosEventos from './MultiplosEventos.vue';
+
   export default {
     name: 'PrimeiroComponente',
+    components: {
+      MultiplosEventos
+    },
     data() {
       return {
         nome: 'Lucas',
