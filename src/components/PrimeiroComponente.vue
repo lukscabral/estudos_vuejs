b743a2c0dbed31562216bf49959ce64c7e9b10c4
lucas <lukscabral95@gmail.com>
2024-06-12 21:50:33 -0300

    reuse

--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -1,16 +1,23 @@
 <template>
   <h1>Olá Vue!</h1>
+  <Reutilizacao/>
+
   <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
   <MultiplosEventos/>
+  <Reutilizacao/>
+  <Reutilizacao/>
+
 </template>
 
 <script>
   import MultiplosEventos from './MultiplosEventos.vue';
+  import Reutilizacao from './Reutilizacao.vue';
 
   export default {
     name: 'PrimeiroComponente',
     components: {
-      MultiplosEventos
+      MultiplosEventos,
+      Reutilizacao
     },
     data() {
       return {
